d9714f100f98c55410a7833100a4863a264dcf13
nfadeluca <nfadeluca@gmail.com>
2023-05-05 23:12:58 -0400

    Added cool route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET cool route */
+router.get('/cool', function(req, res) {
+  res.render('cool');
+});
+
 module.exports = router;
---
+++ views/cool.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './layout';
+
+const Cool = () => {
+  return (
+    <Layout>
+      <h1>You're so cool</h1>
+    </Layout>
+  );
+};
+
+export default Cool;
